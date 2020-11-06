a4c60ad18a17705690e9cf05b364603e7febb421
manavpandya-numantra <71484096+manavpandya-numantra@users.noreply.github.com>
2020-11-06 22:44:21 +0530

    initial commit and schema build done

---
+++ start/server/src/schema.js
@@ -0,0 +1,61 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  # Your schema will go here
+    type Launch {
+        id: ID!
+        site: String
+        mission: Mission
+        rocket: Rocket
+        isBooked: Boolean!
+    }
+
+    type Rocket {
+        id: ID!
+        name: String
+        type: String
+    }
+
+    type User {
+        id: ID!
+        email: String!
+        # Can be empty but not null
+        trips: [Launch]! 
+    }
+
+    type Mission {
+        name: String
+        # Field with query param
+        missionPatch(size: PatchSize): String
+    }
+
+    enum PatchSize {
+        SMALL
+        LARGE
+    }
+
+    # To fetch the data from client (Can't update)
+    type Query {
+        launches: [Launch]!
+        launch(id: ID!): Launch
+        me: User
+    }
+
+    # To allow mutation
+    type Mutation {
+        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+        cancelTrip(launchId: ID!): TripUpdateResponse!
+        login(email: String): String # login token
+    }
+
+    # Define response object type
+    type TripUpdateResponse {
+        success: Boolean!
+        message: String
+        launches: [Launch]
+    }
+
+
+`;
+
+module.exports = typeDefs;
